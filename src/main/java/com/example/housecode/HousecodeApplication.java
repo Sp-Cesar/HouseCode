8159bf6aba74153999f4c9f4bb9e22b492aed88c
cesar <aaronsp966@gmail.com>
2024-07-01 18:43:15 -0500

    Carpeta de Spring

---
+++ src/main/java/com/example/housecode/HousecodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.housecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/housecode/HousecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
